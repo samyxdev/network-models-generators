2ea194546318b9b638523853d906377b65d1a52c
Samy Chouiti <samy.chouiti@grenoble-inp.org>
2022-04-03 14:53:52 +0200

    Converting generator classes to functions

--- generators.py
+++ generators.py
@@ -8,89 +8,72 @@ import random as rd
 get_mle_power = lambda deg_list, max_pow : 1/(np.log(max_pow) - 1/len(deg_list)*sum(np.log(deg_list))) - 1
 get_mle_poisson = lambda deg_list : 1/len(deg_list)*sum(deg_list)
 
-class ModelGenerator(ABC):
+def init_graph(N):
     G = nx.Graph()
+    G.add_nodes_from(range(N))
 
-    N = 0
+    return G
 
-    """
-    Network initialization
-    Paramater checks
-    """
-    def __init__(self, N):
-        self.G.add_nodes_from(range(N))
+def ErdosRenyi(N, K):
+    N = int(N) # For exposant writtings : 1e3 is a float for python
+    G = init_graph(N)
 
-        self.N = int(N)
+    assert 0 <= K <= N*(N-1)/2
 
-    def get_graph(self):
-        return self.G
+    for i in range(K):
+        valid = False
+        n1, n2 = None, None
+        while not valid:
+            n1, n2 = np.random.randint(N), np.random.randint(N)
 
-    def get_deg_list(self):
-        return [d[1] for d in self.G.degree()]
+            if n2 != n1 and not G.has_edge(n1, n2):
+                valid = True
 
-class ErdosRenyi(ModelGenerator):
-    K = 0
+        print("Adding edge:", n1, n2)
+        G.add_edge(n1, n2)
 
-    def __init__(self, N, K):
-        assert 0 <= K <= N*(N-1)/2
-
-        super().__init__(N)
-
-        self.K = int(K)
-
-        for i in range(self.K):
-            valid = False
-            n1, n2 = None, None
-            while not valid:
-                n1, n2 = np.random.choice(self.G.nodes()), np.random.choice(self.G.nodes())
-
-                if n2 != n1 and not self.G.has_edge(n1, n2):
-                    valid = True
-
-            self.G.add_edge(n1, n2)
+    return G
 
 
-class WattsStrogatz(ModelGenerator):
-    k = 0
-    p = 0
+def WattsStrogatz(N, k, p):
+    N = int(N) # For exposant writtings : 1e3 is a float for python
+    G = init_graph(N)
 
-    def __init__(self, N, k, p):
-        assert 0 <= k < N and k%2 == 0
+    assert 0 <= k < N and k%2 == 0
 
-        super().__init__(N)
+    k = int(k)
+    p = p
 
-        self.k = int(k)
-        self.p = p
+    # Initial generation of the regular ring lttice
+    print(nx.number_of_nodes(G))
+    for i in range(N):
+        for j in range(i + 1, (i + k//2 + 1)):
+            #print("C", i, j%N)
+            if(not G.has_edge(i, j%N)):
+                G.add_edge(i, j%N)
 
-        # Initial generation of the regular ring lttice
-        print(nx.number_of_nodes(self.G))
-        for i in range(self.N):
-            for j in range(i + 1, (i + self.k//2 + 1)):
-                #print("C", i, j%self.N)
-                if(not self.G.has_edge(i, j%self.N)):
-                    self.G.add_edge(i, j%self.N)
+    # Rewirings
+    for i in range(N):
+        for nei in range(i + 1, (i + k//2 + 1)):
+            if np.random.choice([False, True], p=[1 - p, p]):
+                #print("Rewriting ", i, nei%N)
+                # If rewiring needed, first remove the rewired edge
+                G.remove_edge(i, nei%N)
 
-        # Rewirings
-        for i in range(self.N):
-            for nei in range(i + 1, (i + self.k//2 + 1)):
-                if np.random.choice([False, True], p=[1 - self.p, self.p]):
-                    #print("Rewriting ", i, nei%self.N)
-                    # If rewiring needed, first remove the rewired edge
-                    self.G.remove_edge(i, nei%self.N)
+                # Then adds an edge with a random node which we are not neighbors with (yet)
+                #print(list(nx.non_neighbors(G, i)))
+                new_nei = np.random.choice(list(nx.non_neighbors(G, i)))
+                #print("New nei:", new_nei)
+                G.add_edge(i, new_nei)
 
-                    # Then adds an edge with a random node which we are not neighbors with (yet)
-                    #print(list(nx.non_neighbors(self.G, i)))
-                    new_nei = np.random.choice(list(nx.non_neighbors(self.G, i)))
-                    #print("New nei:", new_nei)
-                    self.G.add_edge(i, new_nei)
+    return G
 
-
+"""
 class ConfigurationModel(ModelGenerator):
-    """
-    N: Number of nodes
-    deg_dist: Function of the degree distribution. Must have arguments as (law_param, size)
-    law_param: Parameter of the degree distribution
-    """
+
+    # N: Number of nodes
+    # deg_dist: Function of the degree distribution. Must have arguments as (law_param, size)
+    # law_param: Parameter of the degree distribution
     def __init__(self, N, deg_dist, law_param):
         super().__init__(N)
 
@@ -123,3 +106,4 @@ class ConfigurationModel(ModelGenerator):
 
         for i in range(0, len(slots), 2):
             self.G.add_edge(slots[i], slots[i+1])
+"""
--- test_generators.py
+++ test_generators.py
@@ -1,22 +1,30 @@
 from generators import *
 
+import sys
+print(sys.version)
+import networkx as nx
+print(nx.__version__)
+
+
+g1 = ErdosRenyi(20, 5)
+nx.draw(g1, alpha=.5, node_size=20)
+plt.show()
+
+
 """
 
-g1 = ErdosRenyi(50, 50)
-nx.draw(g1.get_graph())
-plt.show()
-
-
 g2 = WattsStrogatz(10, 4, 0.4)
-nx.draw_circular(g2.get_graph(), with_labels=True)
+nx.draw_circular(g2, with_labels=True)
 plt.show()
 
+"""
 """
 g3 = ConfigurationModel(10, np.random.poisson, 5)
 #g3 = ConfigurationModel(10, np.random.power, 5)
 
 print("Estimated power:", get_mle_poisson(g3.get_deg_list()), "Real power:", 3)
 
-nx.draw(g3.get_graph())
+nx.draw(g3)
 plt.show()
 
+"""
